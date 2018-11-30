95bc8cbd15926554e441e79ef7ac38b18f67bf3e
Yatin Vij <ytn.vj2@gmail.com>
2018-11-30 23:48:58 +0530

    Code fixes

--- BikeRentNew.R
+++ BikeRentNew.R
@@ -8,7 +8,8 @@ library(car)
 library(caTools)
 library(rpart)
 library(FNN)
-library(DMwR)
+library(DMwR)#regr. eval
+set.seed(101) 
 # Lets start with loading the data
 df=read.csv('./day.csv')
 # Lets see the structure of the data
@@ -113,32 +114,36 @@ df=outlierImputer(df,num_cols[4:8])
 #    Finding highly correlated numerical features with Count
 # install.packages('corrgram')
 
+# Feature Selection
+
 numFeatureSel=function(df,num_cols){
   corr=cor(df[,num_cols])
   corrgram(df[,num_cols],order = F,upper.panel = panel.pie,text.panel = panel.txt,main='Correlation Plot')
 }
 numFeatureSel(df,num_cols)
-# based on correlation plot we remove the highly correlated variables
+# based on correlation plot we remove the highly correlated variable
 df=subset(df,select = -c(Temp))
-numFeatureSel(df,8:11)
+catFeatureSel=function(df,cat_cols){
+  cSel=NULL
+  for(i in cat_cols){
+    x=summary(aov(Count~df[,i], data = df))
+    print(x)
+    if(x[[1]]$`Pr(>F)`[1]<0.05){
+      cSel=c(cSel,i)
+    }
+  }
+  return(cSel)
+}
+cSel=catFeatureSel(df,cat_cols)
 
-#   
+# Remove Weekday as not significant
+df=subset(df,select = -c(WeekDay))
 
-
-imppred=randomForest(Count ~ ., data = df,ntree = 100, keep.forest = FALSE, importance = TRUE)
-importance(imppred, type = 1)
-df=subset(df,select = -c(Holiday))
-
-#     Feature Scaling
- #Standardisation
- #for(i in 7:10){
- #  df[,i] = (df[,i] - mean(df[,i]))/sd(df[,i])
- #}
+# Convert categorical variables to dummy variables
 df=dummy.data.frame(df, sep = "." )
-df=df[,-c(1,6,26,18)]
-df_scaled = scale(df[,-28])
-df[,-28]=df_scaled
+df=df[,-c(1,6,20)]
 
+# Calculate VIF for all the variables and select variables with low VIF
 VIF_check=function(df_o){
   while (T) {
     lr_model=lm(Count~.,data = df_o)
@@ -154,9 +159,9 @@ VIF_check=function(df_o){
   }
   return(df_o)
 }
-
 df_vif=VIF_check(df)
 
+# Conduct BackElim RandomForest to find important variables
 backwardEliminationRF=function(df,sl){
   numVars = length(df)
   for (i in c(1:numVars)){
@@ -175,11 +180,12 @@ backwardEliminationRF=function(df,sl){
 rf_model=backwardEliminationRF(df_vif,5)
 imp=importance(rf_model)
 
-
-
-df_vif=df_vif[,row.names(imp)]
+# Select only most important variables
+df_vif=df_vif[,c(row.names(imp),'Count')]
 
 df_copy=df
+
+# Conduct Linear BackElim to get most significant variables for linear model
 backwardElimination=function(df,sl){
   numVars = length(df)
   for (i in c(1:numVars)){
@@ -196,18 +202,20 @@ backwardElimination=function(df,sl){
 }
 lr_model=backwardElimination(df_copy,0.01)
 summary(lr_model)
-df_lr=df[,names(lr_model$coefficients)[-1]]
+df_lr=df[,c(names(lr_model$coefficients)[-1],'Count')]
 
-df=df_vif
+# feature scaling
+df_scaled = scale(df_vif[,-19])
+df_vif[,-19]=df_scaled
+
+
 #  Modeling the Data
 # Divide data into train and test using stratified sampling method
 
-set.seed(101) 
-sample = sample.split(df$Count, 600)
-train = subset(df, sample == TRUE)
-test  = subset(df, sample == FALSE)
-
-
+# For Linear BackElim Variables
+sample = sample.split(df_lr$Count, SplitRatio = 0.8)
+train = subset(df_lr, sample == TRUE)
+test  = subset(df_lr, sample == FALSE)
 
 
 #Develop Model on training data
@@ -216,19 +224,26 @@ test  = subset(df, sample == FALSE)
 lr_model=backwardElimination(train,0.05)
 summary(lr_model)
 lr=lm(Count~.,train)
-y_pred=predict(lr_model,test[,1:12])
+y_pred=predict(lr_model,test[,1:14])
 #Summary of Linear model
 summary(lr_model)
-r=sum((y_pred-test[,13])^2)/sum((test[,13]-mean(test[,13]))^2)
-1-r #-> 0.792
+# Visualizing the linear model
+plot(lr_model) 
+r=sum((y_pred-test[,15])^2)/sum((test[,15]-mean(test[,15]))^2)
+1-r #-> 0.819
 
 # Decision Tree Regression Model
 # install.packages('rpart')
 
+# For VIF and RF Variables
+sample = sample.split(df_vif$Count, SplitRatio = 0.8)
+train = subset(df_vif, sample == TRUE)
+test  = subset(df_vif, sample == FALSE)
+
 dt_model=rpart(Count~.,train,method = 'anova')
-y_pred_dt=predict(dt_model,test[,-13])
-r=sum((y_pred_dt-test[,13])^2)/sum((test[,13]-mean(test[,13]))^2)
-1-r #-> 0.792
+y_pred_dt=predict(dt_model,test[,-19])
+r=sum((y_pred_dt-test[,19])^2)/sum((test[,19]-mean(test[,19]))^2)
+1-r #-> 0.657
 summary(dt_model)
 plot(dt_model, uniform = T, branch = 1, margin = 0.05, cex = 0.9)
 text(dt_model, cex = 0.7)
@@ -236,15 +251,16 @@ text(dt_model, cex = 0.7)
 # Random Forest Model
 RF_model = randomForest(Count ~ ., train, importance = TRUE, ntree = 100)
 summary(RF_model)
-y_pred_rf = predict(RF_model, test[,-13])
-r=sum((y_pred_rf-test[,13])^2)/sum((test[,13]-mean(test[,13]))^2)
-1-r #-> 0.834
-# KNN Regression Model
+y_pred_rf = predict(RF_model, test[,-19])
+r=sum((y_pred_rf-test[,19])^2)/sum((test[,19]-mean(test[,19]))^2)
+1-r #-> 0.797
+plot(RF_model)
 
-knn_model=knn.reg(train = train[,-13],test=test[,-13],y=train[,13],k=5)
+# KNN Regression Model
+knn_model=knn.reg(train = train[,-19],test=test[,-19],y=train[,19],k=7)
 y_pred_knn=knn_model$pred
-r=sum((y_pred_knn-test[,13])^2)/sum((test[,13]-mean(test[,13]))^2)
-1-r #-> 0.834
+r=sum((y_pred_knn-test[,19])^2)/sum((test[,19]-mean(test[,19]))^2)
+1-r #-> 0.832
 
 # Error Metrics
 
@@ -253,21 +269,14 @@ MAPE = function(y, yhat){
   mean(abs((y - yhat)/y))
 }
 
-MAPE(test[,11],y_pred_dt)
-MAPE(test[,11],y_pred)
-MAPE(test[,11],y_pred_rf)
+MAPE(test[,19],y_pred_dt)
+MAPE(test[,19],y_pred)
+MAPE(test[,19],y_pred_rf)
 
-regr.eval(trues = test[,11],preds = y_pred,train.y = train[,11],stats = c('mae','mse','rmse','mape','nmse','nmae'))
-regr.eval(trues = test[,11],preds = y_pred_rf,train.y = train[,11],stats = c('mae','mse','rmse','mape','nmse','nmae'))
-regr.eval(trues = test[,11],preds = y_pred_dt,train.y = train[,11],stats = c('mae','mse','rmse','mape','nmse','nmae'))
-regr.eval(trues = test[,11],preds = y_pred_knn,train.y = train[,11],stats = c('mae','mse','rmse','mape','nmse','nmae'))
-
-plot(lr_model)
-
-plot(RF_model)
-
-
-
+regr.eval(trues = test[,19],preds = y_pred,train.y = train[,19],stats = c('mae','mse','rmse','mape','nmse','nmae'))
+regr.eval(trues = test[,19],preds = y_pred_rf,train.y = train[,19],stats = c('mae','mse','rmse','mape','nmse','nmae'))
+regr.eval(trues = test[,19],preds = y_pred_dt,train.y = train[,19],stats = c('mae','mse','rmse','mape','nmse','nmae'))
+regr.eval(trues = test[,19],preds = y_pred_knn,train.y = train[,19],stats = c('mae','mse','rmse','mape','nmse','nmae'))
 
 predict_y=function(model,df,type){
   # Convert the Variables that are factors but were taken as numeric by R
@@ -280,15 +289,15 @@ predict_y=function(model,df,type){
   colnames(df)=c('Instant','Date','Season','Year','Month','Holiday','WeekDay','WorkingDay','WeatherSituation','Temp','ActualTemp','Humidity','WindSpeed','Casual','Registered')
   print('Sample Input')
   print(df[3,])
-  X=subset(df,select = -c(Instant,Date,Registered,Casual,Temp,Holiday))
+  X=subset(df,select = -c(Instant,Date,Registered,Casual,Temp,WeekDay))
   X=dummy.data.frame(X,sep='.')
-  X=X[,-c(1,6,18,26)]
-  X[,] = scale(X, center=attr(df_scaled, "scaled:center"),scale=attr(df_scaled, "scaled:scale"))
+  X=X[,-c(1,6,20)]
+  #X[,] = scale(X, center=attr(df_scaled, "scaled:center"),scale=attr(df_scaled, "scaled:scale"))
   if(type=='linear'){
-    X=X[,-c(2,5:10,13:22)]
+    X=X[,-c(5,7:10,14,15,17)]
   }
   else{
-    X=X[,-c(2,7:10,13:22)]
+    X=X[,-c(2,16,17,20)]
   }
   return(predict(model, X[3,]))
 }
